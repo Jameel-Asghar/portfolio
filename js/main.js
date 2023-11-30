017af10a68c7d11fd0ef72e5198081e2202755a5
Jameel-Asghar <khwajajamil786@gmail.com>
2023-11-30 12:42:39 +0500

    30-11-2023-2

--- js/main.js
+++ js/main.js
@@ -3,7 +3,27 @@
  	easing: 'slide'
  });
 
+ $(document).ready(function () {
+	// Add smooth scrolling to all links inside the navbar
+	$("#home-section a").on('click', function (event) {
+	  if (this.hash !== "") {
+		// Prevent default anchor click behavior
+		event.preventDefault();
 
+		// Store hash
+		var hash = this.hash;
+
+		// Using jQuery's animate() method to add smooth page scroll
+		$('html, body').animate({
+		  scrollTop: $(hash).offset().top
+		}, 800, function () {
+
+		  // Add hash (#) to URL when done scrolling (default click behavior)
+		  window.location.hash = hash;
+		});
+	  }
+	});
+  });
 
  
     $(document).ready(function () {
